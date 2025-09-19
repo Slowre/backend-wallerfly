7029cf9633a0b18b5ccbd8e68d517a5ead805259
Erding Flores <israelflores408@gmail.com>
2025-09-18 21:58:22 -0500

    status response

--- index.js
+++ index.js
@@ -2,22 +2,116 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 const PORT = 3030
 
-app.post('/api/evento/', (req, res) => {
-    res.send('crear')
+let eventos =
+    [
+        {
+            name: "nachos",
+            description: "comida",
+            amount: 50,
+            date: "2025-07-20",
+            type: "Egreso",
+            id: "8cc9d81e-53cc-4c35-886e-5535e8a6766f"
+        },
+        {
+            name: "Conciento",
+            description: "",
+            amount: 40,
+            date: "2025-07-16",
+            type: "Egreso",
+            id: "3a79b226-a888-479b-a3ee-9f46190f8d66"
+        },
+        {
+            name: "sueldo 1",
+            description: "",
+            amount: 1000,
+            date: "2025-07-31",
+            type: "Ingreso",
+            id: "04e7d798-7e7a-4cc0-acad-c4dbc3a73e5b"
+        },
+        {
+            name: "Ventas",
+            description: "",
+            amount: 100,
+            date: "2025-07-10",
+            type: "Ingreso",
+            id: "da7686a9-7664-4f2c-a7a0-3ccff21579ef"
+        },
+        {
+            name: "Compra",
+            description: "",
+            amount: 50,
+            date: "2025-07-22",
+            type: "Egreso",
+            id: "ea7712e7-9ff1-4798-8b35-31307729b213"
+        },
+        {
+            name: "Arriendo",
+            description: "",
+            amount: 100,
+            date: "2025-08-06",
+            type: "Egreso",
+            id: "742e58b6-e530-42bd-9ff5-3d9e511b0e4d"
+        }
+    ]
+
+app.post('/api/eventos/', (req, res) => {
+    const { name, description, amount, date, type, id } = req.body
+    const newEvento = { name, description, amount, date, type, id }
+    eventos.push(newEvento)
+    res.json({ code: 'OK', message: 'Evento creado excitosamente!', data: { evento: newEvento } })
 })
-app.get('/api/evento/', (req, res) => {
-    res.send('listar')
+
+app.get('/api/eventos/', (req, res) => {
+    res.json({ code: 'OK', message: 'Eventos disponibles', data: { eventos } })
 })
-app.get('/api/evento/:id', (req, res) => {
-    res.send('obtener por id')
+
+
+app.get('/api/eventos/query/', (req, res) => {
+    const id = req.query.id
+    if (!id) {
+        return res.status(400).json({ code: 'PF', message: 'El ID de evento es requerido!' })
+    }
+    const evento = eventos.find((e) => e.id = id)
+    if (!evento) {
+        return res.status(404).json({ code: 'NF', message: 'Evento no encontrado!' })
+    }
+    res.json({ code: 'OK', message: 'Eventos disponibles', data: { evento } })
 })
-app.put('/api/evento/:id', (req, res) => {
-    res.send('actualizar')
+
+app.put('/api/eventos/:id', (req, res) => {
+    const { id } = req.params
+    if (!id) {
+        return res.status(400).json({ code: 'PF', message: 'El ID de evento es requerido!' })
+    }
+    const evento = eventos.find((e) => e.id = id)
+    if (evento) {
+        const { name, description, amount, date, type } = req.body
+        evento.amount = amount
+        evento.date = date
+        evento.description = description
+        evento.name = name
+        evento.type = type
+        res.json({ code: 'OK', message: 'Evento actualizado exitosamente!', data: { evento } })
+        return
+    }
+    res.status(404).json({ code: 'NF', message: 'Evento no encontrado!' })
 })
-app.delete('/api/evento/:id', (req, res) => {
-    res.send('eliminar')
+
+app.delete('/api/eventos/:id', (req, res) => {
+    const { id } = req.params
+    if (!id) {
+        return res.status(400).json({ code: 'PF', message: 'El ID de evento es requerido!' })
+    }
+    const evento = eventos.find((e) => e.id == id)
+    if (evento) {
+        eventos = eventos.filter((e) => e.id != evento.id)
+        return res.json({ code: 'OK', message: 'Evento eliminado!', data: { eventoSearch } })
+    }
+    res.status(404).json({ code: 'PF', message: 'Evento no encontrado!' })
 })
 
 app.listen(PORT, () => {
